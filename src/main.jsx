8c6274ef40c4cf9faa1726daf96cf384fd512f54
Samuel Joos <sjoos@prophets.be>
2022-07-03 19:28:04 +0200

    PropTypes

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import PropTypes from 'prop-types';
 
-function Message({text}){
-	return <div>{text}</div>
+function Message({text}) {
+    return <div>{text}</div>
 }
+
+Message.propTypes = {
+    text: PropTypes.string.isRequired
+}
+
+// Message.propTypes = {
+//     text: (props, propName, componentName) => {
+//         if(typeof props[propName] !== 'string') {
+//             return new Error(`${propName} in ${componentName} must be a string`)
+//         }
+//     }
+// }
 
 
 const element = <Message text="bla"/>
