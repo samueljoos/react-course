5b722918bd8fb519b677809619c1af03b605d962
Samuel Joos <sjoos@prophets.be>
2022-07-03 20:35:14 +0200

    conditional styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import clsx from 'clsx';
 import './App.css'
 
-function Button({className, ...rest}) {
-    return <button className={ clsx('button', className) } {...rest}>Submit</button>
+function Button({className, italic=false, ...rest}) {
+    return <button className={ clsx('button', className, italic && 'button-italic') } {...rest}>
+        Submit
+    </button>
 }
 
 function App() {
     return <>
-        <Button className='button—small'  style={{ backgroundColor: 'red' }} />
+        <Button className='button—small' italic={true}  style={{ backgroundColor: 'red' }} />
         <Button className='button—medium'  style={{ backgroundColor: 'blue' }} />
         <Button className='button—large'  style={{ backgroundColor: 'green' }} />
     </>
