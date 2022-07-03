512f4b3cf51741e9ec95760432d69a9d18d73c89
Samuel Joos <sjoos@prophets.be>
2022-07-03 19:24:35 +0200

    JSX

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-const firstElement = React.createElement('div', null, 'first element')
-const secondElement = React.createElement('div', null, 'second element')
-
-//ReactDOM.createRoot(root).render(firstElement, secondElement) //❌ dit gaat niet werken
+const element = <div id="root-div">hello world</div>
+ReactDOM.createRoot(root).render(element)
