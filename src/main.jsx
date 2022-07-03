ee45ed911501affb77c4940a53213dee2dbe240e
Samuel Joos <sjoos@prophets.be>
2022-07-03 19:31:29 +0200

    Interpolation

--- src/main.jsx
+++ src/main.jsx
@@ -4,22 +4,12 @@ import App from './App'
 import './index.css'
 import PropTypes from 'prop-types';
 
-function Message({text}) {
-    return <div>{text}</div>
+function Message({username, text, commentCount}) {
+    return <div>
+        {username}: {text} has {commentCount} comments
+    </div>
 }
 
-Message.propTypes = {
-    text: PropTypes.string.isRequired
-}
 
-// Message.propTypes = {
-//     text: (props, propName, componentName) => {
-//         if(typeof props[propName] !== 'string') {
-//             return new Error(`${propName} in ${componentName} must be a string`)
-//         }
-//     }
-// }
-
-
-const element = <Message text="bla"/>
+const element = <Message username="ioioio" text="Hello world" commentCount={10}/>
 ReactDOM.createRoot(root).render(element)
