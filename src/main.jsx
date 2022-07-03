7752c32a0737f4507cb96c06d065b1d48ea84dbf
Samuel Joos <sjoos@prophets.be>
2022-07-03 19:32:09 +0200

    Interpolation if else

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,11 @@ import './index.css'
 import PropTypes from 'prop-types';
 
 function Message({username, text, commentCount}) {
-    return <div>
-        {username}: {text} has {commentCount} comments
+    const countLabel = commentCount.length ? commentCount : 'no'
+    return <div> 
+        {username}: {text} has {countLabel} comments
     </div>
 }
-
 
 const element = <Message username="ioioio" text="Hello world" commentCount={10}/>
 ReactDOM.createRoot(root).render(element)
