fe08944bef20b06cbdf548c42404ca2b4a4aa857
Samuel Joos <sjoos@prophets.be>
2022-07-03 20:49:11 +0200

    Side effects > useEffect add dependency array

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
 import { useEffect, useState } from 'react';
 import './App.css'
 
-function App() {
+function Greeting() {
     const [name, setName] = useState(() => window.localStorage.getItem('name') || '');
 
-    useEffect(() => {    // deze functie word aangeroepen bij iedere render van het component
+    useEffect(() => {
+        console.log('useEffect update'); // Dit wijzigt nu enkel indien name veranderd
         window.localStorage.setItem('name', name);
-    })
+    }, [name])
 
     return (
-    <div>
-        <label htmlFor='name'>Name</label>
-        <input value={name} id="name" type="text" onChange={() => {
-            setName(event.target.value);
-        }} />
-        <div>Hi <strong>{name}</strong>!</div>
-    </div>
+        <div>
+            <label htmlFor='name'>Name</label>
+            <input value={name} id="name" type="text" onChange={() => {
+                setName(event.target.value);
+            }} />
+            <div>Hi <strong>{name}</strong>!</div>
+        </div>
+    )
+}
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <button onClick={() => setCount(c => c + 1)}>{count}</button>
+            <Greeting/>
+        </div>
     )
 }
 
