b12bb9bc347181fc2563cfc47dfb50058aa5df5b
Samuel Joos <sjoos@prophets.be>
2022-07-04 14:21:16 +0200

    Async data with fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,37 @@
 import gsap from 'gsap';
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import './App.css'
 
 function App() {
-    const [show, setShow] = useState(false);
+    const [q, setQ] = useState('');
+    const [data, setData] = useState([]);
     
-    const ref = useRef();
-    
-    useEffect(() => {     
-        // met ref.currrent haal je het div html element op.
-        gsap.to(ref.current, {
-            duration: 1,
-            opacity: show ? 1: 0,
-            rotate: show ? 180: 0,
-        })// met gsap kunnen we er een animatie aan vast hangen
-    }, [show]);
+    const fetchData = useCallback(async (q) => {
+        if(q.length) {
+            const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${q}&format=json`);
+            const d = await response.json();
+            setData(d);
+        }
+    },[])
+
+    useEffect(() => {
+        fetchData(q);
+    }, [q])
+
     return (
         <>
-        <label>
             <input
-            type="checkbox"
-            checked={show}
-            onChange={e => setShow(e.target.checked)}
+                type="text"
+                onChange={e => setQ(e.target.value)}
             />
-            {' '}show child
-        </label>
-        <div
-            ref={ref}
-            style={{
-                padding: 10,
-                margin: 10,
-                height: 30,
-                width: 30,
-                border: 'solid',
-            }}
-        >
-        
-        </div>
+            
+            {
+                data.map(item => (
+                    <div key={item.place_id}>
+                        <h1>{item.display_name}</h1>
+                    </div>
+                ))
+            }
         </>
     )
 }
