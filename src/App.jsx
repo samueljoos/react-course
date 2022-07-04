e13394fe25e0ecec064e2fffb362d6726b982edc
Samuel Joos <sjoos@prophets.be>
2022-07-04 14:24:58 +0200

    Async data with useSWR

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
 import gsap from 'gsap';
 import { useCallback, useEffect, useRef, useState } from 'react';
+import useSWR from 'swr';
 import './App.css'
+
+const fetcher = (...props) => fetch(...props).then(res => res.json());
 
 function App() {
     const [q, setQ] = useState('');
-    const [data, setData] = useState([]);
     
-    const fetchData = useCallback(async (q) => {
-        if(q.length) {
-            const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${q}&format=json`);
-            const d = await response.json();
-            setData(d);
-        }
-    },[])
-
-    useEffect(() => {
-        fetchData(q);
-    }, [q])
+    const {data, error} =  useSWR(`https://nominatim.openstreetmap.org/search?q=${q}&format=json`, fetcher)
 
     return (
         <>
@@ -26,7 +18,7 @@ function App() {
             />
             
             {
-                data.map(item => (
+                data?.map(item => (
                     <div key={item.place_id}>
                         <h1>{item.display_name}</h1>
                     </div>
